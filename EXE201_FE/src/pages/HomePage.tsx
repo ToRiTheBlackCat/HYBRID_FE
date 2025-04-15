0cb94cfd3228190dbf4dd225249977467a0c0937
DucThang <taducthangcoi123@gmail.com>
2025-04-15 23:27:25 +0700

    add source

---
+++ EXE201_FE/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/HomePage/Header";
+import "../tailwind.css";
+
+const HomePage : React.FC = () =>{
+    return(
+        <>
+            <div>
+                <Header/>
+            </div>
+        </>
+    )
+}
+export default HomePage;
